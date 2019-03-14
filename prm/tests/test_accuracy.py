5baa8408578c402eeb346cabf75456e1a89f4164
Maciej "Iwan" Iwanowski <maciej.iwanowski@intel.com>
2019-03-14 17:58:24 +0100

    Simplified calculate_components() test

--- prm/tests/test_accuracy.py
+++ prm/tests/test_accuracy.py
@@ -17,10 +17,10 @@
 
 
 from json import loads
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 from os.path import dirname, join
+from requests import Response
 
-import prm
 import pytest
 from prm.accuracy import build_prometheus_url, calculate_components, calculate_precision_and_recall
 
@@ -47,104 +47,6 @@ def _dict_from_json(path):
 
 
 @pytest.mark.parametrize(
-    'anomalies,fetched_data,build_prometheus_url_calls,expected_true_positives,'
-    'expected_anomalies_found,expected_real_positives',
-    [
-        # 3 anomalies, no matching SLO violations and 6 SLO violations overall
-        (
-                _dict_from_json('accuracy/anomalies.json'),
-                [
-                    _dict_from_json('accuracy/slo_violations.json'),
-                    _dict_from_json('accuracy/empty_results.json'),
-                    _dict_from_json('accuracy/empty_results.json'),
-                    _dict_from_json('accuracy/empty_results.json'),
-                ],
-                [
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484541.644,
-                      "tags": {"workload_instance": "cassandra_stress--default--14--9142"}}],
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484741.644,
-                      "tags": {"workload_instance": "twemcache_mutilate--big--14--11213"}}],
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484941.644,
-                      "tags": {"workload_instance": "twemcache_mutilate--big--14--11213"}}],
-                ], 0, 3, 6),
-        # 3 anomalies, 1 matching SLO violation and 6 SLO violations overall
-        (
-                _dict_from_json('accuracy/anomalies.json'),
-                [
-                    _dict_from_json('accuracy/slo_violations.json'),
-                    _dict_from_json('accuracy/slo_violation.json'),
-                    _dict_from_json('accuracy/empty_results.json'),
-                    _dict_from_json('accuracy/empty_results.json'),
-                ],
-                [
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484541.644,
-                      "tags": {"workload_instance": "cassandra_stress--default--14--9142"}}],
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484741.644,
-                      "tags": {"workload_instance": "twemcache_mutilate--big--14--11213"}}],
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484941.644,
-                      "tags": {"workload_instance": "twemcache_mutilate--big--14--11213"}}],
-                ], 1, 3, 6),
-        # 3 anomalies, 2 matching SLO violation and 6 SLO violations overall
-        (
-                _dict_from_json('accuracy/anomalies.json'),
-                [
-                    _dict_from_json('accuracy/slo_violations.json'),
-                    _dict_from_json('accuracy/slo_violation.json'),
-                    _dict_from_json('accuracy/slo_violation.json'),
-                    _dict_from_json('accuracy/empty_results.json'),
-                ],
-                [
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484541.644,
-                      "tags": {"workload_instance": "cassandra_stress--default--14--9142"}}],
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484741.644,
-                      "tags": {"workload_instance": "twemcache_mutilate--big--14--11213"}}],
-                    [('irrelevant', 'sli>slo', 1),
-                     {"window_size": 10, "event_time": 1552484941.644,
-                      "tags": {"workload_instance": "twemcache_mutilate--big--14--11213"}}],
-                ], 2, 3, 6),
-        # no anomalies, no matching SLO violation and 6 SLO violations overall
-        (
-                _dict_from_json('accuracy/empty_results.json'),
-                [
-                    _dict_from_json('accuracy/slo_violations.json'),
-                ],
-                [], 0, 0, 6),
-    ])
-def test_calculate_components(anomalies, fetched_data, build_prometheus_url_calls,
-                              expected_true_positives, expected_anomalies_found,
-                              expected_real_positives):
-    prm.accuracy.fetch_metrics = Mock(side_effect=fetched_data)
-    prm.accuracy.build_prometheus_url = Mock()
-    true_positives, anomalies_found, real_positives = calculate_components(anomalies, 'irrelevant',
-                                                                           1, 10)
-
-    assert true_positives == expected_true_positives
-    assert anomalies_found == expected_anomalies_found
-    assert real_positives == expected_real_positives
-
-    # Assert that URL used to fetch all SLO violations is build as expected
-    prm.accuracy.build_prometheus_url.assert_any_call('irrelevant', 'sli>slo', 1)
-    for call in build_prometheus_url_calls:
-        # Assert that URL used to fetch SLO violations for an anomaly is build as expected
-        prm.accuracy.build_prometheus_url.assert_any_call(*call[0],
-                                                          event_time=call[1]['event_time'],
-                                                          window_size=call[1]['window_size'],
-                                                          tags=call[1]['tags'])
-
-    # Assert that build_prometheus_url() if no anomalies are found
-    if len(build_prometheus_url_calls) == 0:
-        prm.accuracy.build_prometheus_url.assert_called_once()
-
-
-@pytest.mark.parametrize(
     'true_positives,anomaly_count,slo_violations,expected_precision,expected_recall', [
         (0, 0, 6, -1, 0),
         (1, 2, 1, 0.5, 1),
@@ -159,3 +61,75 @@ def test_calculate_precision_and_recall(true_positives, anomaly_count, slo_viola
 
     assert precision == expected_precision
     assert recall == expected_recall
+
+
+@pytest.mark.parametrize(
+    'anomalies,fetched_data,expected_true_positives,expected_anomalies_found,'
+    'expected_real_positives',
+    [
+        # 3 anomalies, no matching SLO violations and 6 SLO violations overall
+        (
+            _dict_from_json('accuracy/anomalies.json'),
+            {
+                'irrelevant/api/v1/query?query=sli>slo{build_number="1"}':
+                    _dict_from_json('accuracy/slo_violations.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"cassandra_stress--default--14--9142"}&start=1552484536.644&end=1552484546.644&'
+                'step=1s': _dict_from_json('accuracy/empty_results.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"twemcache_mutilate--big--14--11213"}&start=1552484736.644&end=1552484746.644&'
+                'step=1s': _dict_from_json('accuracy/empty_results.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"twemcache_mutilate--big--14--11213"}&start=1552484936.644&end=1552484946.644&'
+                'step=1s': _dict_from_json('accuracy/empty_results.json'),
+            }, 0, 3, 6),
+        # 3 anomalies, 1 matching SLO violation and 6 SLO violations overall
+        (
+            _dict_from_json('accuracy/anomalies.json'),
+            {
+                'irrelevant/api/v1/query?query=sli>slo{build_number="1"}':
+                    _dict_from_json('accuracy/slo_violations.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"cassandra_stress--default--14--9142"}&start=1552484536.644&end=1552484546.644&'
+                'step=1s': _dict_from_json('accuracy/slo_violation.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"twemcache_mutilate--big--14--11213"}&start=1552484736.644&end=1552484746.644&'
+                'step=1s': _dict_from_json('accuracy/empty_results.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"twemcache_mutilate--big--14--11213"}&start=1552484936.644&end=1552484946.644&'
+                'step=1s': _dict_from_json('accuracy/empty_results.json'),
+            }, 1, 3, 6),
+        # 3 anomalies, 2 matching SLO violation and 6 SLO violations overall
+        (
+            _dict_from_json('accuracy/anomalies.json'),
+            {
+                'irrelevant/api/v1/query?query=sli>slo{build_number="1"}':
+                    _dict_from_json('accuracy/slo_violations.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"cassandra_stress--default--14--9142"}&start=1552484536.644&end=1552484546.644&'
+                'step=1s': _dict_from_json('accuracy/slo_violation.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"twemcache_mutilate--big--14--11213"}&start=1552484736.644&end=1552484746.644&'
+                'step=1s': _dict_from_json('accuracy/slo_violation.json'),
+                'irrelevant/api/v1/query_range?query=sli>slo{build_number="1",workload_instance='
+                '"twemcache_mutilate--big--14--11213"}&start=1552484936.644&end=1552484946.644&'
+                'step=1s': _dict_from_json('accuracy/empty_results.json'),
+            }, 2, 3, 6),
+        # no anomalies, no matching SLO violation and 6 SLO violations overall
+        (
+                _dict_from_json('accuracy/empty_results.json'),
+                {
+                    'irrelevant/api/v1/query?query=sli>slo{build_number="1"}':
+                        _dict_from_json('accuracy/slo_violations.json')
+                }, 0, 0, 6),
+    ])
+def test_calculate_components(anomalies, fetched_data, expected_true_positives,
+                              expected_anomalies_found, expected_real_positives):
+    with patch('prm.accuracy.get', Mock(side_effect=lambda url: Mock(Response,
+                                        json=Mock(return_value=fetched_data[url])))):
+        true_positives, anomalies_found, real_positives = calculate_components(anomalies,
+                                                                               'irrelevant', 1, 10)
+
+    assert true_positives == expected_true_positives
+    assert anomalies_found == expected_anomalies_found
+    assert real_positives == expected_real_positives
