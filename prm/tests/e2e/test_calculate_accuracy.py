2e81d56d3e9c08db74c66a21b92b06beb553583f
Pawel Palucki <pawel.palucki@intel.com>
2019-04-17 19:40:33 +0200

    reorg

--- prm/tests/e2e/test_calculate_accuracy.py
+++ prm/tests/e2e/test_calculate_accuracy.py
@@ -17,33 +17,12 @@
 
 
 import os
-
-from argparse import ArgumentParser
-import requests
+import logging
 
 from prm.accuracy import (build_prometheus_url, fetch_metrics,
                           calculate_components, calculate_precision_and_recall)
 
-import logging
-
-
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('build_number', type=int, help="Jenkins build number",
-                        metavar='BUILD_NUMBER')
-    parser.add_argument('--prometheus', type=str, default="http://127.0.0.1:9090",
-                        help="Prometheus server base URL, eg: http://127.0.0.1:9090")
-    parser.add_argument('--window-size', type=float, default=10.0,
-                        help="Size of time window used to find SLO violations for each anomaly")
-    args = parser.parse_args()
-
-    url = build_prometheus_url(args.prometheus, 'anomaly', args.build_number)
-    anomalies = fetch_metrics(url)
-    true_positives, anomaly_count, slo_violations = calculate_components(
-        anomalies, args.prometheus, args.build_number, args.window_size)
-    precision, recall = calculate_precision_and_recall(
-        true_positives, anomaly_count, slo_violations)
-    print(precision, recall)
+import requests
 
 
 def _get_mesos_running_tasks(mesos_master_host):
@@ -60,35 +39,34 @@ def _get_mesos_running_tasks(mesos_master_host):
         return sorted([t['name'] for t in tasks['get_tasks']['tasks']])
 
 
-def test_workloads_are_running():
-    assert 'MESOS_MASTER_HOST' in os.environ
-    assert 'MESOS_EXPECTED_TASKS' in os.environ
+def test_integration_accurracy(record_property):
+    """ Integration tests to check number of runnings tasks during scenario
+    and calculate and output them to csv file for visulization. """
+    assert 'MESOS_MASTER_HOST' in os.environ, 'required to get number of running tasks'
+    assert 'MESOS_EXPECTED_TASKS' in os.environ, 'required to check number of tasks running'
+    assert 'PROMETHEUS' in os.environ, 'prometheus host to connect'
+    assert 'BUILD_NUMBER' in os.environ, 'build number to find proper metircs'
 
     mesos_master_host = os.environ['MESOS_MASTER_HOST']
-    mesos_expected_tasks = int(os.environ['MESOS_EXPECTED_TASKS'])
-
-    tasks = _get_mesos_running_tasks(mesos_master_host)
-
-    logging.debug('found tasks: %s', tasks)
-    if len(tasks) != mesos_expected_tasks:
-        logging.warning('invalid number of tasks: %r (expected=%r)', len(tasks),
-                        mesos_expected_tasks)
-
-
-def test_integration_accurracy(record_property):
-    assert 'PROMETHEUS' in os.environ
-    assert 'BUILD_NUMBER' in os.environ
-
     prometheus = os.environ['PROMETHEUS']
     build_number = int(os.environ['BUILD_NUMBER'])
-    record_property('build_number', build_number)
-    window_size = 10.0
+    mesos_expected_tasks = int(os.environ['MESOS_EXPECTED_TASKS'])
+    window_size = float(os.environ.get('WINDOW_SIZE', 10.0))
 
     logging.info('window size = %s', window_size)
     logging.info('build number = %r', build_number)
-    prometheus_query = build_prometheus_url(prometheus, 'anomaly', dict(build_number=build_number))
-    logging.info('prometheus query = %r', prometheus_query)
-    anomalies = fetch_metrics(prometheus_query)
+
+    # Check running tasks.
+    tasks = _get_mesos_running_tasks(mesos_master_host)
+    logging.info('tasks = %s', len(tasks))
+    assert len(tasks) > mesos_expected_tasks, \
+        'invalid number of tasks: %r (expected=%r)' % (len(tasks), mesos_expected_tasks)
+
+    # Calculate results.
+    prometheus_anomalies_query = build_prometheus_url(
+        prometheus, 'anomaly', dict(build_number=build_number))
+    logging.debug('prometheus query = %r', prometheus_anomalies_query)
+    anomalies = fetch_metrics(prometheus_anomalies_query)
     logging.info('found anomalies = %s', len(anomalies))
 
     true_positives, anomaly_count, slo_violations = calculate_components(
@@ -100,21 +78,17 @@ def test_integration_accurracy(record_property):
     precision, recall = calculate_precision_and_recall(
         true_positives, anomaly_count, slo_violations)
 
+    record_property('build_number', build_number)
     record_property('recall', recall)
     record_property('precision', precision)
+    record_property('tasks', tasks)
+
     logging.info('recall = %s', recall)
     logging.info('precision = %s', precision)
 
-    if not os.path.exists('test_results.csv'):
-        with open('test_results.csv', 'w') as f:
-            f.write('recall,precision\n')
-
-    with open('test_results.csv', 'a') as f:
-        f.write('%s,%s\n' % (recall, precision))
+    with open('test_results.csv', 'w') as f:
+        f.write('recall,precision,tasks\n')
+        f.write('%s,%s,%s\n' % (recall, precision, len(tasks)))
 
     assert precision > 0
     assert recall > 0
-
-
-if __name__ == "__main__":
-    main()
