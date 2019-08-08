848e4d81fd5a95f1b57e9c3302c91fe1006bb7a4
WangJialei-A <jialei.a.wang@intel.com>
2019-08-08 14:36:04 +0800

    WIP

--- prm/prm/detector.py
+++ prm/prm/detector.py
@@ -127,6 +127,7 @@ class ContentionDetector(detectors.AnomalyDetector):
 
         cid = con.cid
         thresh = self._get_thresholds(app, ThreshType.METRICS)
+        print(self.threshs)
         print(app, ThreshType.METRICS, thresh, sep=' ')
         if thresh:
             contends, wca_metrics = con.contention_detect(thresh)
